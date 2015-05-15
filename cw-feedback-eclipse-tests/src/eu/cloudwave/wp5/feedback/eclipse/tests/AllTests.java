ee711c7239b1dd10797e0f40cc8cfd2a3681823f
Dang Nguyen <haidang.4513@gmail.com>
2015-05-15 11:13:20 +0200

    add missing dependency org.eclipse.equinox.event in tests

--- cw-feedback-eclipse-tests/src/eu/cloudwave/wp5/feedback/eclipse/tests/AllTests.java
+++ cw-feedback-eclipse-tests/src/eu/cloudwave/wp5/feedback/eclipse/tests/AllTests.java
@@ -24,6 +24,6 @@ import org.junit.runners.Suite.SuiteClasses;
 @RunWith(Suite.class)
 @SuiteClasses({//@formatter:off
   AllNonSwtBotTests.class,
-  AllSwtBotTests.class,
+  AllSwtBotTests.class
 })//@formatter:on
 public class AllTests {}
